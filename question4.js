128567b1a6019f0e12eac80d73c76495c5656134
TheodorLindvall <theodor.lindvall@chasacademy.se>
2024-12-18 14:26:37 +0100

    Update question4.js

--- question4.js
+++ question4.js
@@ -1,18 +1,7 @@
-// 4. Destructuring (3p)
-// Använd destructuring för att plocka ut värdena namn och stad från objektet person.
-// Skriv en funktion greeting som tar namn och stad som parametrar och returnerar en sträng:
-// "Hej, jag heter [namn] och bor i [stad]."
+const person = ["Hej jag heter Anna", "och är från Stockholm"];
 
-const person = {
-  name: "Anna",
-  age: 25,
-  stad: "Stockholm",
-};
+console.log(person);
 
-function greeting(name, city) {
-  // Din kod här:
-}
 
-// Plocka ut name och city med destructuring här:
 
-// Anropa funktionen greeting här (den ska returnera "Hej, jag heter Anna och bor i Stockholm" ):
+
