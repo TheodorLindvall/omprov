d9c24ba66066af3601d24f75be20183b98268756
TheodorLindvall <theodor.lindvall@chasacademy.se>
2024-12-18 14:41:20 +0100

    Update question4.js

--- question4.js
+++ question4.js
@@ -1,7 +1,15 @@
-const person = ["Hej jag heter Anna", "och är från Stockholm"];
+const person = {
+    name: "Anna",
+    age: 25,
+    stad: "Stockholm",
+};
 
-console.log(person);
+function greeting(name, city) {
+    return "Hej, jag heter {name} och bor i {city}";
+}
 
+const { name, stad: city } = person;
 
+console.log(greeting(name, city)); 
 
 
